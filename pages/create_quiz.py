33c2e8739ca26628f177023f2945f583a077915a
ryudama-private <my_com_address@yahoo.co.jp>
2026-03-29 08:42:56 +0900

    画像アップロードできるようにするまで

--- pages/create_quiz.py
+++ pages/create_quiz.py
@@ -1,7 +1,17 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title="問題作成")
 st.title("問題作成")
+
+uploaded = st.file_uploader(
+"画像をアップロードしてください",
+type=["png", "jpg", "jpeg", "webp"]
+)
+
+if uploaded is not None:
+    img = Image.open(uploaded)
+    st.image(img, caption="アップロード画像", use_container_width=True)
 
 with st.sidebar:
     st.header("メニュー")
